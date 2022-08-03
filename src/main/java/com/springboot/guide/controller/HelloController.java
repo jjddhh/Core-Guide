2e65a65d7a5d8884719c2275827b911401b64b0a
ehdgh <ehdgh25@naver.com>
2022-08-03 13:18:47 +0900

    db 연결 및 swggaer 설정 추가

--- src/main/java/com/springboot/guide/controller/HelloController.java
+++ src/main/java/com/springboot/guide/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.springboot.guide.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
-    @RequestMapping("/hello")
-    public String hello() {
-        return "Hello World";
+    @GetMapping("/hello")
+    public ResponseEntity<?> hello() {
+
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
