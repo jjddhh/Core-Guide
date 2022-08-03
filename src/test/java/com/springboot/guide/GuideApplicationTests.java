fe8b3b44e0412c7e03d7e546311d160af809354a
ehdgh <ehdgh25@naver.com>
2022-08-03 10:37:32 +0900

    초기설정

---
+++ src/test/java/com/springboot/guide/GuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
